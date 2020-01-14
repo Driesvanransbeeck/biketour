c2f4527da8ff82254e5eaae40b12e9599d0e307f
Dries Van Ransbeeck <driesvanransbeeck@gmail.com>
2020-01-14 18:00:25 +0100

    add final message Tokyo to map

--- app.jsx
+++ app.jsx
@@ -8,67 +8,33 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 
 // Markers
 
-var marker = L.marker([50.8503396, 4.351710300000036]).addTo(map);
-marker.bindPopup("<b>Starting point!</b><br>Departure on Sunday 3 February").openPopup();
+var Brussels = L.marker([50.8503396, 4.351710300000036]).bindPopup("<b>Starting point!</b><br>Departure on Sunday 3 February"),
+    Munich = L.marker([48.1351, 11.5820]).bindPopup('<a href="https://medium.com/road-to-the-rising-sun/were-off-the-beginning-of-a-new-chapter-60fb434c6771" target="_blank"><b>We’re off! The beginning of a new chapter</b></a><br>17 February in Munich: First blog post'),
+    Bratislava = L.marker([48.1486, 17.1077]).bindPopup('<a href="https://medium.com/road-to-the-rising-sun/bye-bye-to-western-europe-hello-to-the-east-e76549c9f94a?fbclid=IwAR0K2A50F23NcOvo1wyIlqQWmmsMSbB60Hisw0o-HbxS-Bjrnsyel2FLpnk" target="_blank"><b>Bye bye to Western Europe, hello to the East!</b></a><br>3 March in Bratislava: blog post with video'),
+    Belgrade = L.marker([44.7866, 20.4489]).bindPopup('<a href="https://medium.com/road-to-the-rising-sun/fewer-cycling-paths-more-fascinating-inside-stories-and-stray-dogs-af11f5567bc1" target="_blank"><b>Fewer cycling paths, more fascinating inside stories and stray dogs</b></a><br>18 March in Belgrade: blog post with video'),
+    Sofia = L.marker([42.6977, 23.3219]).bindPopup('<a href="https://medium.com/road-to-the-rising-sun/impressed-by-the-beauty-of-the-balkans-cycling-from-belgrade-to-sofia-239482c65aca" target="_blank"><b>Impressed by the beauty of the Balkans: cycling from Belgrade to Sofia</b></a><br>31 March in Sofia: blog post with video'),
+    Izmir = L.marker([38.4237, 27.1428]).bindPopup('<a href="https://medium.com/road-to-the-rising-sun/turkey-the-country-that-made-us-change-our-travel-itinerary-199e32a3b0bd" target="_blank"><b>Turkey: the country that made us change our travel itinerary</b></a><br>1 May in Izmir: blog post'),
+    Silopi = L.marker([37.3055, 42.4983]).bindPopup('<a href="https://medium.com/road-to-the-rising-sun/the-end-of-our-2-unforgettable-months-in-turkey-d959fcf43f79" target="_blank"><b>The end of our 2 unforgettable months in Turkey!</b></a><br>29 May in Silopi: blog post with video'),
+    Sulaymaniyah = L.marker([35.5558, 45.4351]).bindPopup('<a href="https://medium.com/road-to-the-rising-sun/exploring-the-unthinkable-iraqi-kurdistan-f98960587747" target="_blank"><b>Exploring the unthinkable: Iraqi Kurdistan</b></a><br>5 June in Sulaymaniyah: blog post'),
+    Mashhad = L.marker([36.2605, 59.6168]).bindPopup('<a href="https://medium.com/road-to-the-rising-sun/cycling-through-iran-a-very-intense-yet-extremely-rewarding-experience-b1c43f29591b" target="_blank"><b>Cycling through Iran: a very intense yet extremely rewarding experience</b></a><br>9 July in Mashhad: blog post'),
+    Turkmenabad = L.marker([39.0041, 63.5688]).bindPopup('<a href="https://medium.com/road-to-the-rising-sun/tour-of-turkmenistan-race-against-time-5db84b508a06" target="_blank"><b>Tour of Turkmenistan: race against time</b></a><br>15 July in Turkmenabad: blog post'),
+    Osh = L.marker([40.5140, 72.8161]).bindPopup('<a href="https://medium.com/road-to-the-rising-sun/one-month-of-nature-in-its-purest-form-in-tajikistan-kyrgyzstan-dbcd5d310e9b" target="_blank"><b>One month of nature in its purest form in Tajikistan & Kyrgyzstan</b></a><br>18 August in Osh: blog post + video'),
+    Bishkek = L.marker([42.8746, 74.5698]).bindPopup('<a href="https://velotourfestival.be/" target="_blank"><b>Great news! We are going to organise the first Velo Tour Festival in Belgium next year!</b></a><br>Announcement: 1 September in Bishkek'),
+    Guilin = L.marker([25.2345, 110.1800]).bindPopup('<a><b>First-hand experience of the Great Firewall of China: impossible to publish blog posts</b></a><br>15 October in Guilin: digital detox'),
+    Beijing = L.marker([39.9042, 116.4074]).bindPopup('<a href="https://medium.com/road-to-the-rising-sun/china-a-country-with-multiple-faces-our-journey-and-impressions-on-the-bicycle-4c38e9a638ab" target="_blank"><b>China, a country with multiple faces: our journey and impressions on the bicycle</b></a><br>29 October in Beijing: blog post'),
+    Busan = L.marker([35.1796, 129.0756]).bindPopup('<a href="https://medium.com/road-to-the-rising-sun/autumn-cycling-in-south-korea-4c754d76fb71" target="_blank"><b>Autumn cycling in South Korea</b></a><br>14 November in Busan: blog post');
 
-var marker = L.marker([48.1351, 11.5820]).addTo(map);
-marker.bindPopup('<a href="https://medium.com/road-to-the-rising-sun/were-off-the-beginning-of-a-new-chapter-60fb434c6771" target="_blank"><b>We’re off! The beginning of a new chapter</b></a><br>17 February in Munich: First blog post').openPopup();
+var posts = L.layerGroup([Brussels, Munich, Bratislava, Belgrade, Sofia, Izmir, Silopi, Sulaymaniyah, Mashhad, Turkmenabad, Osh, Bishkek, Guilin, Beijing, Busan]);
 
-var marker = L.marker([48.1486, 17.1077]).addTo(map);
-marker.bindPopup('<a href="https://medium.com/road-to-the-rising-sun/bye-bye-to-western-europe-hello-to-the-east-e76549c9f94a?fbclid=IwAR0K2A50F23NcOvo1wyIlqQWmmsMSbB60Hisw0o-HbxS-Bjrnsyel2FLpnk" target="_blank"><b>Bye bye to Western Europe, hello to the East!</b></a><br>3 March in Bratislava: blog post with video').openPopup();
-
-var marker = L.marker([44.7866, 20.4489]).addTo(map);
-marker.bindPopup('<a href="https://medium.com/road-to-the-rising-sun/fewer-cycling-paths-more-fascinating-inside-stories-and-stray-dogs-af11f5567bc1" target="_blank"><b>Fewer cycling paths, more fascinating inside stories and stray dogs</b></a><br>18 March in Belgrade: blog post with video').openPopup();
-
-var marker = L.marker([42.6977, 23.3219]).addTo(map);
-marker.bindPopup('<a href="https://medium.com/road-to-the-rising-sun/impressed-by-the-beauty-of-the-balkans-cycling-from-belgrade-to-sofia-239482c65aca" target="_blank"><b>Impressed by the beauty of the Balkans: cycling from Belgrade to Sofia</b></a><br>31 March in Sofia: blog post with video').openPopup();
-
-var marker = L.marker([38.4237, 27.1428]).addTo(map);
-marker.bindPopup('<a href="https://medium.com/road-to-the-rising-sun/turkey-the-country-that-made-us-change-our-travel-itinerary-199e32a3b0bd" target="_blank"><b>Turkey: the country that made us change our travel itinerary</b></a><br>1 May in Izmir: blog post').openPopup();
-
-var marker = L.marker([37.3055, 42.4983]).addTo(map);
-marker.bindPopup('<a href="https://medium.com/road-to-the-rising-sun/the-end-of-our-2-unforgettable-months-in-turkey-d959fcf43f79" target="_blank"><b>The end of our 2 unforgettable months in Turkey!</b></a><br>29 May in Silopi: blog post with video').openPopup();
-
-var marker = L.marker([35.5558, 45.4351]).addTo(map);
-marker.bindPopup('<a href="https://medium.com/road-to-the-rising-sun/exploring-the-unthinkable-iraqi-kurdistan-f98960587747" target="_blank"><b>Exploring the unthinkable: Iraqi Kurdistan</b></a><br>5 June in Sulaymaniyah: blog post').openPopup();
-
-var marker = L.marker([36.2605, 59.6168]).addTo(map);
-marker.bindPopup('<a href="https://medium.com/road-to-the-rising-sun/cycling-through-iran-a-very-intense-yet-extremely-rewarding-experience-b1c43f29591b" target="_blank"><b>Cycling through Iran: a very intense yet extremely rewarding experience</b></a><br>9 July in Mashhad: blog post').openPopup();
-
-var marker = L.marker([39.0041, 63.5688]).addTo(map);
-marker.bindPopup('<a href="https://medium.com/road-to-the-rising-sun/tour-of-turkmenistan-race-against-time-5db84b508a06" target="_blank"><b>Tour of Turkmenistan: race against time</b></a><br>15 July in Turkmenabad: blog post').openPopup();
-
-var marker = L.marker([40.5140, 72.8161]).addTo(map);
-marker.bindPopup('<a href="https://medium.com/road-to-the-rising-sun/one-month-of-nature-in-its-purest-form-in-tajikistan-kyrgyzstan-dbcd5d310e9b" target="_blank"><b>One month of nature in its purest form in Tajikistan & Kyrgyzstan</b></a><br>18 August in Osh: blog post + video').openPopup();
-
-var marker = L.marker([42.8746, 74.5698]).addTo(map);
-marker.bindPopup('<a href="https://velotourfestival.be/" target="_blank"><b>Great news! We are going to organise the first Velo Tour Festival in Belgium next year!</b></a><br>Announcement: 1 September in Bishkek').openPopup();
-
-var marker = L.marker([25.2345, 110.1800]).addTo(map);
-marker.bindPopup('<a><b>First-hand experience of the Great Firewall of China: impossible to publish blog posts</b></a><br>15 October in Guilin: digital detox').openPopup();
-
-var marker = L.marker([39.9042, 116.4074]).addTo(map);
-marker.bindPopup('<a href="https://medium.com/road-to-the-rising-sun/china-a-country-with-multiple-faces-our-journey-and-impressions-on-the-bicycle-4c38e9a638ab" target="_blank"><b>China, a country with multiple faces: our journey and impressions on the bicycle</b></a><br>29 October in Beijing: blog post').openPopup();
-
-var marker = L.marker([35.1796, 129.0756]).addTo(map);
-marker.bindPopup('<a href="https://medium.com/road-to-the-rising-sun/autumn-cycling-in-south-korea-4c754d76fb71" target="_blank"><b>Autumn cycling in South Korea</b></a><br>14 November in Busan: blog post').openPopup();
-
-var Istanbul = L.marker([41.0082376, 28.97835889999999]).bindPopup("This is Istanbul!"),
-    Tbilisi = L.marker([41.7151377, 44.82709599999998]).bindPopup("This is Tbilisi!"),
-    Tehran = L.marker([35.6891975, 51.388973599999986]).bindPopup("This is Tehran!"),
-    Bishkek = L.marker([42.8746212, 74.56976170000007]).bindPopup("This is Bishkek!"),
-    Tokyo = L.marker([35.708409939046184, 139.73015854843743]).bindPopup("This is Tokyo!");
-
-var cities = L.layerGroup([Istanbul, Tbilisi, Tehran, Bishkek, Tokyo]);
-
-map.addLayer(cities);
+map.addLayer(posts);
 
 var overlayMaps = {
-    "Cities we will take some rest": cities
+    "Our blog posts": posts
 };
 
 L.control.layers(null, overlayMaps, { collapsed: false }).addTo(map);
 
+/*
 // Itinerary
 
 function getColor(m) {
@@ -157,6 +123,8 @@ info.update = function (props) {
 
 info.addTo(map);
 
+*/
+
 // real GPX data
 
 var gpx_options = {
@@ -180,6 +148,24 @@ new L.GPX("GPX/Osh-Dali.gpx", gpx_options).addTo(map);
 new L.GPX("GPX/China-Seoul.gpx", gpx_options).addTo(map);
 new L.GPX("GPX/Seoul-Nagasaki.gpx", gpx_options).addTo(map);
 new L.GPX("GPX/Nagasaki-Tokyo.gpx", gpx_options).addTo(map);
+
+// data collected during our travel
+
+/*
+
+new L.GPX("GPX/Recommendations-locals.gpx", {
+  async: true,
+  marker_options: {
+    shadowUrl: 'https://image.flaticon.com/icons/svg/616/616489.svg',
+    shadowSize: [15, 14]
+  }
+}).on('loaded', function (e) {
+  var gpx = e.target;
+  map.fitBounds(gpx.getBounds());
+}).addTo(map);
+
+*/
+
 /*
 
 new L.GPX("GPX/1. Brussels-Namur.gpx", gpx_options).addTo(map);
@@ -251,4 +237,5 @@ var bikeIcon = L.icon({
     iconSize: [40, 40], // size of the icon
 });
 
-L.marker([35.6762, 139.6503], { icon: bikeIcon }).addTo(map);
+var marker = L.marker([35.6762, 139.6503], { icon: bikeIcon }).addTo(map);
+marker.bindPopup('<b>WE MADE IT TO TOKYO! 🇯🇵</b><p>What started out as a crazy idea turned out to be the journey of a lifetime across different cultures - a journey that would not have been possible without all the amazing people we met on our way. 🙏🏻<p>We feel incredibly thankful to everyone who was part of this intense journey. Now it is our turn to welcome cyclists and other travelers at our place. You can <a href="https://www.warmshowers.org/users/driesvr" target="_blank"><b>find us on Warmshowers</b></a>. Happy cycling! ❤️<p>P.S.: You are all invited to the <a href="https://www.velotourfestival.be" target="_blank"><b>VeloTour Festival</b></a> we are going to organise in Belgium from 1 to 3 May 2020.').openPopup();
